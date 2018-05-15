b4006edad8d033caa451d0aaa27e22d078f13429
S. M. Hickman <sean@grump.ly>
2018-05-14 19:11:35 -0500

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,9 @@
+{ mkDerivation, base, stdenv }:
+mkDerivation {
+  pname = "pure-ease";
+  version = "0.7.0.0";
+  src = ./.;
+  libraryHaskellDepends = [ base ];
+  homepage = "github.com/grumply/pure-ease";
+  license = stdenv.lib.licenses.bsd3;
+}
